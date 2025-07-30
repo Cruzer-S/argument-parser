89d3af64a20385fce09712a7a720ce61f470d469
Cruzer-S <yyyynoom@gmail.com>
2025-07-31 08:15:21 +0900

    shorthand

--- source/main.c
+++ source/main.c
@@ -6,36 +6,38 @@
 int main(int argc, char *argv[])
 {
 	ArgumentParser parser;
-	ArgumentValue name, flag, port;
+	char *name;
+	bool flag;
+	int port;
 
 	parser = argument_parser_create(argv);
 	if (parser == NULL)
 		exit(EXIT_FAILURE);
 
-	name.s = "default name";
-	flag.b = false;
-	port.i = 1584;
+	name = "default name";
+	flag = false;
+	port = 1584;
 
 	argument_parser_add(
-		parser, "n", "name", "this is name", &name,
+		parser, "n", "name", "this is name", (ArgumentValue *) &name,
 		ARGUMENT_PARSER_TYPE_STRING
 	);
 
 	argument_parser_add(
-		parser, "f", "flag", "this is flag", &flag,
+		parser, "f", "flag", "this is flag", (ArgumentValue *) &flag,
 		ARGUMENT_PARSER_TYPE_BOOLEAN
 	);
 
 	argument_parser_add(
-		parser, "p", "port", "this is port number", &port,
+		parser, "p", "port", "this is port number", (ArgumentValue *) &port,
 		ARGUMENT_PARSER_TYPE_INTEGER
 	);
 
 	argument_parser_parse(parser);
 
-	printf("name: %s\n", name.s);
-	printf("flag_on: %s\n", flag.b ? "on" : "off");
-	printf("port: %d\n", port.i);
+	printf("name: %s\n", name);
+	printf("flag_on: %s\n", flag ? "on" : "off");
+	printf("port: %d\n", port);
 
 	argument_parser_destroy(parser);
 
